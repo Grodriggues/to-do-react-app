c35650d84226d1440798d06ebf1c658b46b496f1
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2020-05-09 15:22:32 -0300

    Updates

---
+++ precache-manifest.be68e856f3dd247775a8549c283c5a6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b57e4def2fdaae58a7c365c17c0bc2",
+    "url": "/to-do-react-app/index.html"
+  },
+  {
+    "revision": "638088fcd3012bd320c1",
+    "url": "/to-do-react-app/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "946adae28a86b86e8421",
+    "url": "/to-do-react-app/static/css/main.0eebe7a5.chunk.css"
+  },
+  {
+    "revision": "638088fcd3012bd320c1",
+    "url": "/to-do-react-app/static/js/2.0439f727.chunk.js"
+  },
+  {
+    "revision": "109083572dc3ba74f105a9f6f5c9e305",
+    "url": "/to-do-react-app/static/js/2.0439f727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946adae28a86b86e8421",
+    "url": "/to-do-react-app/static/js/main.571b02ba.chunk.js"
+  },
+  {
+    "revision": "0f6495c2240d466004f6",
+    "url": "/to-do-react-app/static/js/runtime-main.3c1d4975.js"
+  }
+]);
